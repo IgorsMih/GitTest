baa276e970b280874e0ff60fc500fced6b333577
Igors Mihejevs <mihejevs.igors@gmail.com>
2020-04-23 09:43:39 +0300

    First GitTest Commit

---
+++ InchToMeterTable.java
@@ -0,0 +1,23 @@
+/*
+    This program displays a conversion
+    table of inches to meters.
+*/
+class InchToMeterTable {
+    public static void main (String[] args) {
+        double inches, meters, feet;
+        int counter;
+        for (feet = 1; feet <= 12; feet++) {
+            counter = 0;
+            for (inches = 1; inches <= feet * 12; inches++)  {
+                meters = inches * 39.37;
+                System.out.println(inches + " inches is " +
+                                   meters + " meters.");
+                counter++;
+                if (counter == 12) {
+                System.out.println();
+                counter = 0;
+                }
+            }
+        }
+    }
+}
